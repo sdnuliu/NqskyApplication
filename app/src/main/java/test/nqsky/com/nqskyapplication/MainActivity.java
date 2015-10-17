16f43c96a9c6953a311708fd939a08fe33d1698b
liuteng <sdnuliu@qq.com>
2015-10-17 23:35:31 +0800

    增加自定义分割线

--- app/src/main/java/test/nqsky/com/nqskyapplication/MainActivity.java
+++ app/src/main/java/test/nqsky/com/nqskyapplication/MainActivity.java
@@ -45,6 +45,28 @@ public class MainActivity extends AppCompatActivity {
         return datas;
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
     private class NewAdapter extends RecyclerView.Adapter<ViewHolder> {
 
         private String[] datas = null;
@@ -69,28 +91,6 @@ public class MainActivity extends AppCompatActivity {
         public int getItemCount() {
             return datas.length;
         }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     class ViewHolder extends RecyclerView.ViewHolder {
